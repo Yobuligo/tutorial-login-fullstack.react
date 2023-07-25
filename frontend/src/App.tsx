0531d86ac399bc3cf12dd1848cec0ff86418cd8a
Peter <hoffmannpeter82@gmail.com>
2023-07-25 12:59:30 +0200

    Display Login as ModalDialog

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { LoginDialog } from "./features/login/loginDialog/LoginDialog";
+import { ModalDialog } from "./features/modalDialog/ModalDialog";
 
 const App: React.FC = () => {
+  const [showModalDialog, setShowModalDialog] = useState(false);
   return (
     <>
-      <LoginDialog />
+      {showModalDialog && (
+        <ModalDialog>
+          <LoginDialog />
+        </ModalDialog>
+      )}
+      <button
+        onClick={() => {
+          console.log("Display modal dialog");
+          setShowModalDialog(true);
+        }}
+      >
+        This a a test button
+      </button>
     </>
   );
 };
